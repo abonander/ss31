ffbbb1e2ccd39043f88f0ced36195a882a78c2ae
AlmostPete <almostpete@gmail.com>
2015-03-01 16:49:21 -0700

    Added Scene and SceneManager, moved Logger

---
+++ Client/Scene/Scene.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace SS31
+{
+	// Base class that represents a "scene" in the game, only one can be active at a time.
+	// For allowing different states, i.e. "Main Menu" -> "Multiplayer Menu" -> "In Game" ect.
+	public abstract class Scene : IDisposable
+	{
+		public string TypeName { get { return GetType().ToString(); } }
+
+		#region Members
+		public abstract bool ShouldSuspend { get; } // If the scene should be suspended and cached instead of disposed.
+
+		// If the scene has been initialized
+		private bool _isInitialized;
+		public bool IsInitialized
+		{
+			get
+			{
+				if (IsDisposed)
+					throw new ObjectDisposedException("Scene");
+				return _isInitialized;
+			}
+		}
+		// If the scene is the current active scene, mutually exclusive with IsSuspended
+		private bool _isCurrent;
+		public bool IsCurrent
+		{
+			get
+			{
+				if (IsDisposed)
+					throw new ObjectDisposedException("Scene");
+				return _isCurrent;
+			}
+		}
+		// If the scene is suspended, as it was active but ShouldSuspend was true, so it was cached instead of disposed
+		private bool _isSuspended;
+		public bool IsSuspended
+		{
+			get
+			{
+				if (IsDisposed)
+					throw new ObjectDisposedException("Scene");
+				return _isSuspended;
+			}
+		}
+
+		private bool _isDisposed;
+		public bool IsDisposed { get { return _isDisposed; } }
+
+		private SSClient _game;
+		public SSClient Game
+		{
+			get
+			{
+				if (IsDisposed)
+					throw new ObjectDisposedException("Scene");
+				return _game;
+			}
+			set
+			{
+				if (_game != null)
+					throw new InvalidOperationException("Cannot set the Scene's game more than once.");
+				_game = value;
+			}
+		}
+		#endregion
+
+		protected Scene()
+		{
+			_isInitialized = false;
+			_isCurrent = false;
+			_isSuspended = false;
+			_isDisposed = false;
+			_game = null;
+		}
+		~Scene()
+		{
+			Dispose(false);
+		}
+
+		// Nothing outside of SceneManager should ever call the next three functions
+		public void DoInitialize()
+		{
+			if (_isInitialized)
+				throw new Exception("The state " + TypeName + " was already initialized when Initialize() was called.");
+
+			ServiceManager.Resolve<Profiler>().BeginBlock("Scene Initialize (" + TypeName + ")");
+			Initialize();
+			ServiceManager.Resolve<Profiler>().EndBlock();
+			_isInitialized = true;
+		}
+		public void DoSuspend()
+		{
+			if (!ShouldSuspend)
+				throw new Exception("The state " + TypeName + " should not be suspended, but instead should be disposed.");
+			if (_isSuspended)
+				throw new Exception("The state " + TypeName + " was already suspended when Suspend() was called.");
+
+			ServiceManager.Resolve<Profiler>().BeginBlock("Scene Suspend (" + TypeName + ")");
+			Suspend();
+			ServiceManager.Resolve<Profiler>().EndBlock();
+			_isSuspended = true;
+			_isCurrent = false;
+		}
+		public void DoResume()
+		{
+			if (!ShouldSuspend)
+				throw new Exception("The state " + TypeName + " should not be suspended, and therefore should never be resumed.");
+			if (!_isSuspended)
+				throw new Exception("The state " + TypeName + " was not suspended when Resume() was called.");
+
+			ServiceManager.Resolve<Profiler>().BeginBlock("Scene Resume (" + TypeName + ")");
+			Resume();
+			ServiceManager.Resolve<Profiler>().EndBlock();
+			_isSuspended = false;
+			_isCurrent = true;
+		}
+
+		protected abstract void Initialize(); // Called when the scene is first initialized
+		public virtual void PreUpdate(GameTime gameTime) { } // Called before Update() for optional pre-update actions
+		public abstract void Update(GameTime gameTime); // Called when the scene is updated
+		public virtual void PreDraw(GameTime gameTime) { } // Called after Update() and before Draw(), for any pre-drawing actions
+		public abstract void Draw(GameTime gameTime); // Called when the scene is drawn
+		protected virtual void Suspend() { } // If DisposeOnLeave is false, this will be called before the scene is cached
+		protected virtual void Resume() { } // If this scene is suspended, this will be called when it is moved from the cache to the active scene
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		public void Dispose(bool disposing)
+		{
+			if (IsDisposed)
+				return;
+
+			_game = null;
+			_isDisposed = true;
+		}
+	}
+}
---
+++ Client/Scene/SceneManager.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace SS31
+{
+	public class SceneManager : Service
+	{
+		#region Members
+		private Dictionary<Type, Scene> _cachedScenes;
+		private SSClient _client; // The client to create new scenes with
+		private Profiler _profiler;
+
+		private bool _changeQueued; // If a scene change will be made at the end of this frame.
+		private Type _typeQueued; // The type of scene that will be switched to.
+
+		public Scene ActiveScene { get; private set; }
+		public bool HasScene { get { return ActiveScene != null; } }
+		#endregion
+
+		public SceneManager()
+		{
+			_cachedScenes = new Dictionary<Type, Scene>();
+			_changeQueued = false;
+			_typeQueued = null;
+			ActiveScene = null;
+			_profiler = ServiceManager.Resolve<Profiler>();
+		}
+
+		public void Initialize(SSClient c)
+		{
+			if (_client != null)
+				return;
+
+			_client = c;
+		}
+
+		public bool SwitchTo<T>()
+		{
+			if (_client == null)
+				throw new InvalidOperationException("The client being passed to Scenes from SceneManager cannot be null.");
+
+			Type type = typeof(T);
+			bool cached = _cachedScenes.ContainsKey(type);
+
+			if (ActiveScene != null && ActiveScene.GetType() == type)
+				throw new ArgumentException("Cannot change the active scene to a scene of the same type.");
+
+			_changeQueued = true;
+			_typeQueued = type;
+			return cached;
+		}
+
+		public bool HasCachedType<T>() where T : Scene
+		{
+			return _cachedScenes.ContainsKey(typeof(T));
+		}
+		public void DeleteCachedType<T>() where T : Scene
+		{
+			Type type = typeof(T);
+			if (!_cachedScenes.ContainsKey(type))
+				throw new ArgumentException("The scene manager cannot delete the requested scene type, because it is not cached.");
+
+			Scene delete = _cachedScenes[type];
+			_cachedScenes.Remove(type);
+			delete.Dispose();
+		}
+		public Scene GetCachedType<T>() where T : Scene
+		{
+			Type type = typeof(T);
+
+			if (_cachedScenes.ContainsKey(type))
+				return _cachedScenes[type];
+
+			return null;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (ActiveScene != null)
+			{
+				_profiler.BeginBlock("Scene PreUpdate");
+				ActiveScene.PreUpdate(gameTime);
+				_profiler.EndBlock();
+				_profiler.BeginBlock("Scene Update");
+				ActiveScene.Update(gameTime);
+				_profiler.EndBlock();
+			}
+		}
+		public void Draw(GameTime gameTime)
+		{
+			if (ActiveScene != null)
+			{
+				_profiler.BeginBlock("Scene PreDraw");
+				ActiveScene.PreDraw(gameTime);
+				_profiler.EndBlock();
+				_profiler.BeginBlock("Scene Draw");
+				ActiveScene.Draw(gameTime);
+				_profiler.EndBlock();
+			}
+
+			if (_changeQueued)
+				doSwitch();
+		}
+
+		private void doSwitch()
+		{
+			if (!_changeQueued || _typeQueued == null)
+				throw new Exception("Cannot do a scene switch, invalid arguments for next scene.");
+
+			_profiler.BeginBlock("Scene Switch");
+
+			string last = "null";
+			if (ActiveScene != null)
+			{
+				if (ActiveScene.ShouldSuspend)
+					ActiveScene.DoSuspend();
+				else
+				{
+					ActiveScene.Dispose();
+					_cachedScenes.Remove(ActiveScene.GetType());
+				}
+				last = ActiveScene.GetType().ToString();
+			}
+
+			ActiveScene = null;
+
+			if (!_cachedScenes.ContainsKey(_typeQueued))
+			{
+				ConstructorInfo cinfo = _typeQueued.GetConstructors().FirstOrDefault();
+				if (cinfo == null)
+					throw new NoPublicConstructorException(_typeQueued);
+				ParameterInfo[] pinfo = cinfo.GetParameters();
+				if (pinfo.Any())
+					throw new InvalidConstructorParameterException("Subclasses of Scene must have a zero-argument constructor.");
+
+				Scene ns = (Scene)Activator.CreateInstance(_typeQueued);
+				ns.Game = _client;
+				ns.DoInitialize();
+				_cachedScenes[_typeQueued] = ns;
+				ActiveScene = ns;
+			}
+			else
+			{
+				Scene ns = _cachedScenes[_typeQueued];
+				ns.DoResume();
+				ActiveScene = ns;
+			}
+
+			Logger.LogInfo("Changed to the active scene from " + last + " to " + ActiveScene.GetType() + ".");
+
+			_typeQueued = null;
+			_changeQueued = false;
+
+			_profiler.EndBlock();
+		}
+	}
+}
