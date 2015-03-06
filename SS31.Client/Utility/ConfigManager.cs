2f50f418c19e51063ce6728df18899e388d22f9f
AlmostPete <almostpete@gmail.com>
2015-03-06 14:45:55 -0700

    Added System.Xml reference, started configuration

---
+++ SS31.Client/Utility/ConfigManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using SS31.Common.Utility;
+using SS31.Common;
+
+namespace SS31.Client.Utility
+{
+	public class ClientConfigManager : ConfigManager<ClientConfiguration>
+	{
+		public override void Initialize(string file)
+		{
+			if (File.Exists(file))
+			{
+				XmlSerializer loader = new XmlSerializer(typeof(ClientConfiguration));
+				StreamReader reader = File.OpenText(file);
+				ClientConfiguration config = (ClientConfiguration)loader.Deserialize(reader);
+				reader.Close();
+				Configuration = config;
+				ConfigFile = file;
+			}
+			else
+			{
+				Logger.LogError("Could not load the configuration file: \"" + file + "\". Creating default file and using default settings.");
+				Configuration = new ClientConfiguration();
+				ConfigFile = file;
+				Save();
+			}
+		}
+
+		public override void Save()
+		{
+
+		}
+	}
+}
---
+++ SS31.Common/Utility/IConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SS31.Common.Utility
+{
+	public delegate void HandleConfigValueChanged(string name, object oldValue, object newValue);
+
+	public interface IConfiguration
+	{
+		event HandleConfigValueChanged OnValueChanged;
+
+		object GetSetting(string name);
+		void SetSetting(string name, object value);
+	}
+}
