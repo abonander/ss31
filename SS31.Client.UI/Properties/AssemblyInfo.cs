0c616b4b8d23229bbb7023bb9265c4a308816072
AlmostPete <almostpete@gmail.com>
2015-03-09 10:24:40 -0600

    Added project files for the gui library

---
+++ SS31.Client.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ss31gui")]
+[assembly: AssemblyDescription("UI library for the SS31 client.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SS31 Dev Team")]
+[assembly: AssemblyProduct("SS31")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.0.*")]
