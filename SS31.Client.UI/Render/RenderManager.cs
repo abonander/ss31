ac62910b941fbd1077eb0de4a4b9620c96f7a3d8
AlmostPete <almostpete@gmail.com>
2015-03-09 21:03:28 -0600

    First classes for the UI library, the managers

---
+++ SS31.Client.UI/Render/RenderManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SS31.Client.UI
+{
+	internal class RenderManager : IDisposable
+	{
+		#region Members
+		private bool _disposed;
+
+		private RasterizerState _scissorState; // State for activating the scissor test
+
+		internal GraphicsDevice GraphicsDevice { get; private set; } // The graphics device for the application
+		internal SpriteBatch SpriteBatch { get; private set; } // The spritebatch used specifically by this library
+		#endregion
+
+		public RenderManager(GraphicsDevice device)
+		{
+			if (device == null)
+				throw new ArgumentNullException("device");
+
+			GraphicsDevice = device;
+			SpriteBatch = new SpriteBatch(device);
+
+			_scissorState = new RasterizerState { ScissorTestEnable = true };
+
+			_disposed = false;
+		}
+		~RenderManager()
+		{
+			Dispose(false);
+		}
+
+		public void Draw()
+		{
+			SpriteBatch.Begin(
+				SpriteSortMode.Immediate,
+				BlendState.AlphaBlend,
+				SamplerState.PointClamp,
+				DepthStencilState.None,
+				_scissorState
+			);
+
+			// TODO: do the rendering
+
+			SpriteBatch.End();
+
+			// Reset Graphics Device settings to default
+			GraphicsDevice.BlendState = BlendState.Opaque;
+			GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+			GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+			GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+		}
+
+		#region Dispose
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		public void Dispose(bool disposing)
+		{
+			if (!_disposed && disposing)
+			{
+				SpriteBatch.Dispose();
+				SpriteBatch = null;
+				GraphicsDevice = null;
+			}
+
+			_disposed = true;
+		}
+		#endregion
+	}
+}
---
+++ SS31.Client.UI/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using SS31.Common;
+using SS31.Common.Service;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SS31.Client.UI
+{
+	public class UIManager : GameService
+	{
+		#region Members
+		internal RenderManager RenderManager { get; private set; }
+		#endregion
+
+		public UIManager(GraphicsDevice device)
+		{
+			if (device == null)
+				throw new ArgumentNullException("device");
+
+			RenderManager = new RenderManager(device);
+		}
+
+		public void Update(GameTime gameTime)
+		{
+
+		}
+
+		public void Draw()
+		{
+			RenderManager.Draw();
+		}
+
+		#region Dispose
+		public override void Dispose(bool disposing)
+		{
+			// TODO: Dispose things, I suppose
+
+			base.Dispose(disposing);
+		}
+		#endregion
+	}
+}
