8d12a5f3033fe9acf702183c88278be114f51ea3
AlmostPete <almostpete@gmail.com>
2015-03-13 12:35:21 -0600

    Cleaned up \Widget\ and added Layout

--- SS31.Client.UI/Widget/Widget.cs
+++ SS31.Client.UI/Widget/Widget.cs
@@ -7,6 +7,26 @@ using Microsoft.Xna.Framework.Input;
 
 namespace SS31.Client.UI
 {
+	// Base interface for widgets
+	public interface IWidget : IDisposable
+	{
+		IWidgetContainer Parent { get; }
+
+		bool IsFocusedWidget { get; }
+		bool IsHoveredWidget { get; }
+
+		Rectangle InnerArea { get; }
+		Rectangle OuterArea { get; set; }
+
+		Rectangle AbsoluteInnerArea { get; }
+		Rectangle AbsoluteOuterArea { get; }
+
+		Rectangle AbsoluteInputArea { get; }
+
+		Point Position { get; set; }
+		Point AbsolutePosition{ get; }
+	}
+
 	public abstract class Widget : IWidget
 	{
 		private static uint count;
--- SS31.Client.UI/Widget/WidgetContainer.cs
+++ SS31.Client.UI/Widget/WidgetContainer.cs
@@ -6,6 +6,22 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SS31.Client.UI
 {
+	// Interface for widgets that contain other widgets
+	public interface IWidgetContainer : IWidget
+	{
+		// The children of this widget
+		List<Widget> Children { get; }
+		// If this widget should use the stencil buffer to mask its children
+		bool MaskChildren { get; }
+
+		// Add a widget as a child
+		void AddChild(Widget w);
+		// Remove a widget instance as a child
+		void RemoveChild(Widget w);
+		// Remove a widget with the specified identifier as a child
+		void RemoveChild(string ident);
+	}
+
 	// A base class for widgets that contain other widgets, with the default IWidgetContainer implementation
 	public class WidgetContainer : Widget, IWidgetContainer
 	{
