d695f7393ebcf9df8dc50285f4b8430444c4d083
AlmostPete <almostpete@gmail.com>
2015-03-16 16:55:56 -0600

    Rewrote the config system and reimplemented

---
+++ SS31.Client/Config/ClientConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SS31.Common.Config;
+
+namespace SS31.Client.Config
+{
+	public struct ClientConfiguration : IConfiguration
+	{
+		// Width of the screen
+		public ushort ScreenWidth;
+		// Height of the screen
+		public ushort ScreenHeight;
+	}
+}
---
+++ SS31.Common/Config/IConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SS31.Common.Config
+{
+	// Base class for global configuration settings for each application
+	public interface IConfiguration
+	{
+		
+	}
+}
